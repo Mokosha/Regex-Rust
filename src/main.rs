1eaf90955a897e43cdd5e399e3671936994a2510
Pavel Krajcevski <Krajcevski@gmail.com>
2015-12-02 01:53:18 -0500

    Add main fn for command line usage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+extern crate regex;
+
+use regex::regex::SatisfiesRegex;
+
 fn main() {
     let args : Vec<String> = ::std::env::args().collect();
-    println!("{:?}", args);
+    if args.len() != 3 {
+        println!("Usage: <regex> <string>");
+        return;
+    }
+
+    if args[1].clone().is_satisfied_by(args[2].clone()) {
+        println!("Match!");
+    } else {
+        println!("No match!");
+    }
 }
